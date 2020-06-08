43d3b2c5fa32deb3d2109ec5be197d4a8fc9f76d
Daniel Penaloza <danielpenalozatj@gmail.com>
2020-06-08 14:56:06 -0700

    Stateful Widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,17 @@
-import 'package:counter/src/pages/home_page.dart';
+import 'package:counter/src/pages/counter_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
 
 class MyApp extends StatelessWidget {
   // Overriding the method build of stateleswidget
   @override 
   Widget build(context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: 
       Center(
-        child: HomePage(),
+        child: CounterPage(),
       ),
     );
   }
--- lib/src/pages/counter_page.dart
+++ lib/src/pages/counter_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  final styleText = new TextStyle(fontSize: 25);
-  final int counter = 10;
+class CounterPage extends StatefulWidget {
+  @override
+  createState() => new _CounterPageState();
+}
 
+//Private Class
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+  final _styleText = new TextStyle(fontSize: 25);
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Titulo'),
+        title: Text('Stateful'),
         centerTitle: true,
       ),
       body: Center(
@@ -16,15 +22,17 @@ class HomePage extends StatelessWidget {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Numero de clicks: ', style: styleText),
-            Text('$counter', style: styleText),
+            Text('Numero de clicks: ', style: _styleText),
+            Text('$_counter', style: _styleText ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-            print('Hola Mundo');
+            setState(() {
+              _counter++;
+            });
         },
       ),
     );
